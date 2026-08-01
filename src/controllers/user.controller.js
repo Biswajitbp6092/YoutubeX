b3d9b7ab0cc215fb5348dffa694cda2d804c9623
biswajitecd <biswajit@ethicalden.com>
2026-08-01 20:25:28 +0530

    user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,57 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js"
+import { User } from "../models/user.model.js"
+import { uploadOnCloudinary } from "../utils/Cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
 
-export const registerUser = asyncHandler(async(req, res)=>{
-    res.status(200).json({
-        message:"ok"
+export const registerUser = asyncHandler(async (req, res) => {
+    const { userName, email, fullName, password } = req.body;
+
+    if ([userName, email, fullName, password].some((field) => field.trim() === "")) {
+        throw new ApiError(400, "All field are required")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{ userName }, { email }]
     })
+
+    if (existedUser) {
+        throw new ApiError(408, "user name and email already exites")
+    }
+
+    const avatatLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPatch = req.files?.coverImage[0]?.path;
+
+    if (!avatatLocalPath) {
+        throw new ApiError(400, "avatar is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatatLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPatch)
+
+    if (!avatar) {
+        throw new ApiError(400, "Avatar is required")
+    }
+
+    const user = await User.create({
+        username: username.toLowerCase(),
+        email,
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        password,
+    });
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser){
+        throw new ApiError(500,"Somthing went wrong while registering user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered Successfully")
+    )
+
 })

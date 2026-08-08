7e1a42c6f8f0f02a38a263bda8b5ef32664eb4dc
biswajitecd <biswajit@ethicalden.com>
2026-08-08 17:09:40 +0530

    fix: update refreshAccessToken to handle missing refresh token case feat: add subscription model for user channel subscriptions

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -140,7 +140,7 @@ export const logoutUser = asyncHandler(async (req, res) => {
 export const refreshAccessToken = asyncHandler(async (req, res) => {
     const incomingRefreshToken = req.cookie.refreshToken || req.body.refreshToken;
 
-    if (incomingRefreshToken) {
+    if (!incomingRefreshToken) {
         throw ApiError(401, "unauthorized request")
     }
 
